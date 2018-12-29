082b36e0c40b7ef807553844a075dd3ef229054a
mmacy <marsma@microsoft.com>
2018-12-28 17:15:30 -0800

    add unit tests

---
+++ src/tbrpg.Tests/DiceTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tbrpg.Dice;
+
+namespace tbrpg.Tests
+{
+    [TestClass]
+    public class DiceTests
+    {
+        /// <summary>
+        /// Ensures that the <see cref="DiceRoll"/> always returns values within the desired range.
+        /// </summary>
+        [TestMethod][TestCategory("Dice")]
+        public void DiceRollsAlwaysWithinBounds()
+        {
+            int numRolls = 1000;
+
+            int numDie = 1;
+            int numSides = 20;
+            DiceHand hand = new DiceHand(numDie, numSides);
+            DiceRoll roll = new DiceRoll(hand);
+            int result;
+            for (int i = 0; i < numRolls; i++)
+            {
+                result = roll.RollDice();
+                Assert.IsTrue(result >= numDie);
+                Assert.IsTrue(result <= numDie * numSides);
+            }
+
+            numDie = 2;
+            numSides = 10;
+            hand = new DiceHand(numDie, numSides);
+            roll = new DiceRoll(hand);
+            for (int i = 0; i < numRolls; i++)
+            {
+                result = roll.RollDice();
+                Assert.IsTrue(result >= numDie);
+                Assert.IsTrue(result <= numDie * numSides);
+            }
+        }
+
+        [TestMethod][TestCategory("Dice")]
+        [ExpectedException(typeof(ArgumentException), "Invalid DiceHand specified, but exception not thrown.")]
+        public void InvalidDiceRollThrowsException()
+        {
+            int numDie = 1;
+            int numSides = 1; // Can't have a single-sided die
+            DiceHand hand = new DiceHand(numDie, numSides);
+            DiceRoll roll = new DiceRoll(hand);
+        }
+
+        [TestMethod][TestCategory("Dice")]
+        [ExpectedException(typeof(ArgumentNullException), "Invalid DiceRoll constructor specified, but exception not thrown.")]
+        public void DiceRollWithEmptyDieCodeTextConstructorThrowsException()
+        {
+            DiceRoll roll = new DiceRoll("");
+        }
+
+        [TestMethod][TestCategory("Dice")]
+        [ExpectedException(typeof(ArgumentException), "Invalid DiceRoll constructor specified, but exception not thrown.")]
+        public void DiceRollWithInvalidDieCodeTextConstructorThrowsException()
+        {
+            DiceRoll roll = new DiceRoll("BLARGH!");
+        }
+    }
+}
---
+++ src/tbrpg.Tests/PartyGenerator.cs
@@ -0,0 +1,54 @@
+﻿using tbrpg.CoreRules;
+
+namespace tbrpg.Tests
+{
+    public static class PartyGenerator
+    {
+        private static int pc_hp = 30;
+        private static int pc_df = 15;
+
+        private static int mn_hp = 20;
+        private static int mn_df = 10;
+
+        public static Party GetPlayerParty()
+        {
+            Party party = new Party();
+
+            int hp = pc_hp;
+            int def = pc_df;
+
+            party.AddPartyMember(GetBeing("Blarg the Destroyer", hp, def));
+            party.AddPartyMember(GetBeing("Draflonduh", hp, def));
+            party.AddPartyMember(GetBeing("Vizplag", hp, def));
+            party.AddPartyMember(GetBeing("Wangdu", hp, def));
+            party.AddPartyMember(GetBeing("Miss Cinnamon", hp, def));
+            party.AddPartyMember(GetBeing("Fred Garvin", hp, def));
+
+            return party;
+        }
+
+        public static Party GetMonsterParty()
+        {
+            Party party = new Party();
+
+            for (int i = 0; i < 4; i++)
+            {
+                party.AddPartyMember(GetBeing("Monster " + i.ToString(), mn_hp, mn_df));
+            }
+
+            return party;
+        }
+
+        private static Being GetBeing(string name, int hp, int defense)
+        {
+            return new Being
+            {
+                Name = name,
+                Defense = defense,
+                HitPoints = hp,
+                MaxHitPoints = hp,
+                IsTargetable = true
+            };
+        }
+    }
+}
